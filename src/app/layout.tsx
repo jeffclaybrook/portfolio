213c6b3cfc533d10a990e81b790bb607677e0b4e
Jeff Claybrook <jeffclaybrook@Jeffs-MacBook-Pro.local>
2024-07-11 15:38:47 -0500

    Updated font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Plus_Jakarta_Sans } from "next/font/google"
 import { ReactNode } from "react"
 import Footer from "@/components/footer"
 import Navbar from "@/components/navbar"
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
+const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Jeffrey Claybrook",
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={plusJakartaSans.className}>
         <Navbar />
         {children}
         <Footer />
